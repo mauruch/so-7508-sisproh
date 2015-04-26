f0672b7167844c42a4f1568039c48e78f1c4223c
FCancel <fdcancellara@gmail.com>
2015-04-26 13:03:41 -0300

    Agregado primeras lineas del InsPro.sh para hacer a los scripts ejecutables

--- SisProH/InsPro.sh
+++ SisProH/InsPro.sh
@@ -1,1 +1,16 @@
 #Comando de instalacion
+
+#Lo primero que vamos a hacer sería decirle a todos los scripts que son ejecutables
+#De esta forma se los va a poder llamar de esta forma `./Mover.sh`
+
+chmod +x InsPro.sh
+chmod +x Glog.sh
+chmod +x InfPro.pl
+chmod +x IniPro.sh
+chmod +x Mover.sh
+chmod +x ProPro.sh
+chmod +x RecPro.sh
+chmod +x Start.sh
+chmod +x Stop.sh
+
+#Luego faltaría agregarle el archivo de configuracion
