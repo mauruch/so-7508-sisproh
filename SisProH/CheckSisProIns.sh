956f2890d1990b15ef0572547a68f83f4f3a55b1
mauruch <capolupo.mauro@gmail.com>
2015-05-03 22:23:40 -0300

    Falta el checkeo de perl y loguear mas acciones

---
+++ SisProH/CheckSisProIns.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+insproConf=$CONFDIR/InsPro.conf
+head_msg="TP SO7508 Primer Cuatrimestre 2015. Tema H Copyright © Grupo 06"
+
+#if [ -f $insproLog ] || [ $# -eq 1 ]; then
+    echo $head_msg
+	echo -e "\n"
+
+array_key=( "$CONFDIR" "$BINDIR" "$MAEDIR" "$NOVEDIR" "$DATASIZE" "$ACEPDIR" "$RECHDIR" "$PROCDIR" "$INFODIR" "$DUPDIR" "$LOGDIR" "$LOGSIZE" )
+
+array_value=( "Directorio de Configuración" "Directorio de Ejecutables" "Directorio de Maestros y Tablas" "Directorio de recepción de documentos para protocolización" "Espacio mínimo libre para arribos [Mb]" "Directorio de Archivos Aceptados" "Directorio de Archivos Rechazados" "Directorio de Archivos Protocolizados" "Directorio para informes y estadísticas" "Nombre para el repositorio de duplicados" "Directorio para Archivos de Log" "Tamaño máximo para los archivos de log del sistema [Kb]")
+
+elements=${#array_key[@]}
+
+for (( i=0;i<$elements;i++ )); do
+
+	KEY=${array_key[${i}]}
+
+	echo -e "${array_value[${i}]}: $KEY \n"
+
+	#listar los archivos cuando es necesario
+	#if [ "$KEY" = "$CONFDIR" ] || [ "$KEY" = "$BINDIR" ] || [ "$KEY" = "$MAEDIR" ]	|| [ "$KEY" = "$LOGDIR" ] ; then
+	#	ls $KEY
+	#fi
+	
+done
+
+echo -e "\n"
+
+#fi
+
+
+#if perl < /dev/null > /dev/null 2>&1  ; then
+#     perl -v
+#else
+#      echo dang... no perl
+#fi
+
--- SisProH/InsPro.sh
+++ SisProH/InsPro.sh
@@ -12,14 +12,66 @@ chmod +x RecPro.sh
 chmod +x Start.sh
 chmod +x Stop.sh
 
-#Luego faltaría agregarle el archivo de configuracion
+#Funcion para que el usuario ingrese los directorios
+function setDir() {
 
-#!/bin/bash
+keys=$1
+values=$2
+numElements=${#keys[@]}
+
+if [ "$#" -ne 3 ]; then
+	default=$keys
+else
+	default=$3
+fi
+
+for (( i=0;i<11;i++)); do
+
+	eval k=\${$keys[i]}
+	eval v=\${$values[i]}
+	eval d=\${$default[i]}
+
+	if [ "$#" -ne 3 ]; then
+		eval d=\${"$k"}
+	fi
+	
+    echo "Defina $v ($d):"
+	read input
+
+	#si presiona ENTER se seta el default
+	if [ "$input" = "" ]; then
+    	dir="$d"
+	else
+		if [ $k = "DATASIZE" ] || [ $k = "LOGSIZE" ]; then
+			dir="$input"
+		else
+    		dir="$GRUPO/$input"
+		fi
+	fi
+	
+
+	eval $k=$dir
+	eval val=\${"$k"}
+	export $k
+
+	if [ $k = "DATASIZE" ]; then
+		finalSize=$(bash CheckSpaceDisk.sh)
+		eval $k=$finalSize
+		eval val=\${"$k"}
+		#export DATASIZE con el nuevo valor
+		export $k
+	fi
+
+	# se loguea la accion
+	sh Glog.sh "Se estableció $v: $k" "INFO"
+
+done 
+}
+
+#PRINCIPAL
 
 export GRUPO=/home/mauro/developer/grupo06
 export CONFDIR="$GRUPO/conf"
-
-
 
 #checkeo si esta instalado
 #checkInstallation
@@ -28,6 +80,9 @@ export CONFDIR="$GRUPO/conf"
 insproLog="$CONFDIR/InsPro.log"
 if [ ! -f $insproLog ]; then
     touch $insproLog
+else 
+	echo "checkeando instalación"
+#	bash checkSisProIns.sh
 fi
 
 sh Glog.sh "Inicio de la Ejecución de InsPro" "INFO"
@@ -43,32 +98,117 @@ array_value=( "el directorio para los ejecutables" "el directorio para los maest
 
 array_default=( "$GRUPO/bin" "$GRUPO/mae" "$GRUPO/novedades" "100" "$GRUPO/a_protocolizar" "$GRUPO/rechazados" "$GRUPO/protocolizados" "$GRUPO/informes" "$GRUPO/dup" "$GRUPO/log" "400" )
 
-elements=${#array_key[@]}
+initInstallCondition=""
 
-for (( i=0;i<$elements;i++)); do
+#Llamo a la funcion para que el usuario defina los directorios
 
-    echo "Defina ${array_value[${i}]} (${array_default[${i}]}):"
-	read input
-
-	KEY=${array_key[${i}]}
-
-	#si presiona ENTER se seta el default
-	if [ "$input" = "" ]; then
-    	KEY="${array_default[${i}]}"
+numTimes=1
+while [ "$initInstallCondition" != "s" ] 
+do
+	#si se llama por primera vez se usan los valores por default sino los que ya ingreso
+	if [ $numTimes -eq 1 ]; then  
+		setDir array_key array_value array_default
 	else
-    	KEY="$GRUPO/$input"
-	fi
-	echo $KEY
-	#si no existe el dir se crea
-	if [ ! -d $KEY ]; then
-    	mkdir $KEY
+		setDir array_key array_value
 	fi
 
-	# se loguea la accion
-	sh Glog.sh "Se estableció ${array_value[${i}]}: $KEY" "INFO"
+	#limpiar pantalla
+	clear
 
-done 
+	#llamo a la funcion para que imprima las variables
+	bash CheckSisProIns.sh "showVariables"
 
+	echo "Estado de la instalación: LISTA"
+
+	echo "Inicia la instalación? (s/n)"
+
+	read initInstallCondition
+
+	while [ "$initInstallCondition" != "s" ] && [ "$initInstallCondition" != "n" ] 
+	do
+		echo "Ingreso inválido... Inicia la instalación? (s/n)"
+		read initInstallCondition
+	done
+	(( numTimes++ ))
+	
+	if [ "$initInstallCondition" != "s" ]; then
+		#limpiar pantalla
+		clear
+	fi
+
+done
+
+echo "Iniciando Instalación. Esta Ud. seguro? (s/n)"
+read confirmInstall
+
+echo -e "\n"
+
+if [ "$confirmInstall" = "s" ]; then
+	echo "Creando Estructuras de directorio...."
+	
+	for (( i=0;i<11;i++)); do
+		k="${array_key[$i]}"
+		eval finalDir=\${"$k"}
+		
+		if [ "$k" != "DATASIZE" ] || [ "$k" != "LOGSIZE" ]; then	
+			#si no existe el dir se crea
+			if [ ! -d $finalDir ]; then
+    			mkdir $finalDir
+				echo "$finalDir"
+				if [ "$k" = "MAEDIR" ]; then
+					mkdir "$finalDir/tab"
+					echo "$finalDir/tab"
+					mkdir "$finalDir/tab/ant"
+					echo "$finalDir/tab/ant"
+				fi
+				if [ "$k" = "PROCDIR" ]; then
+					mkdir "$finalDir/proc"
+					echo "$finalDir/proc"
+				fi
+			fi
+		fi
+	done
+fi
+
+echo -e "\n"
+echo -e "Instalando Archivos Maestros y Tablas \n"
+
+#Mover los archivos maestros a MAEDIR y las tablas al directorio MAEDIR/tab
+
+dataDir="20115-1C-Datos/"
+
+#Se mueven los archivos maestros
+lsMaeResult=`ls $dataDir | grep '\.mae$'`
+	
+for f in $lsMaeResult; do
+	bash Mover.sh "$dataDir$f" "$MAEDIR" "InsPro.sh"
+done
+
+#Se mueven los archivos tablas
+lsTabResult=`ls $dataDir | grep '\.tab$'`
+
+for f in $lsTabResult; do
+  bash Mover.sh "$dataDir$f" "$MAEDIR/tab" "InsPro.sh"
+done
+
+#Mover los ejecutables y funciones 
+echo -e "Instalando Programas y Funciones \n"
+lsScriptsResult=`ls | grep '\.sh$'`
+currentDirectory=`pwd`
+for f in $lsScriptsResult; docl
+  bash Mover.sh "$currentDirectory/$f" "$BINDIR" "InsPro.sh"
+done
+
+
+echo -e "Actualizando la configuración del sistema \n"
+
+for (( i=0;i<11;i++)); do
+	k="${array_key[$i]}"
+	eval finalDir=\${"$k"}
+	echo "$k=$finalDir" >> $insproLog
+done
+
+echo "Instalación CONCLUIDA"
 
 #instalacion exitosa
 touch $CONFDIR/InsPro.conf
