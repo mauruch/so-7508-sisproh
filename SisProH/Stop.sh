d275cce452b3ba3859cc680e2a584b4c4fe497d0
Aranlu27 <arancibial.fiuba@gmail.com>
2015-05-07 03:00:09 -0300

    Stop funcionando, testeado

--- SisProH/Stop.sh
+++ SisProH/Stop.sh
@@ -1,16 +1,14 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 #Shell script para detener procesos
-	
 function stop(){
-	RECPROPROCESSID=$(/bin/ps -fu $USER | grep "RecPro.sh" | grep -v "grep" | awk '{print $2}')	
+RECPROPROCESSID=$(/bin/ps -fu $USER | grep "RecPro.sh" | grep -v "grep" | awk '{print $2}')
 	if [ $? -eq 0 ]; then
 		echo "RecPro se está ejecutando. Lo vamos a cerrar."
 		kill $RECPROPROCESSID
-	else
+		else
 		echo "RecPro.sh no se estaba ejecutando."
 		echo "El comando RecPro.sh no se estaba ejecutando."
-	fi
-
+		fi
 }
-stop 
+stop
