a30dd0f8c0ee140556315dae89e405099795e261
FCancel <fdcancellara@gmail.com>
2015-05-09 15:25:50 -0300

    Modificado el stop.sh para que termine el proceso sin utilizar sudo

--- SisProH/Stop.sh
+++ SisProH/Stop.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #Shell script para detener procesos
-function stop(){
-RECPROPROCESSID=$(/bin/ps -fu $USER | grep "RecPro.sh" | grep -v "grep" | awk '{print $2}')
-	if [ $? -eq 0 ]; then
-		echo "RecPro se está ejecutando. Lo vamos a cerrar."
-		kill $RECPROPROCESSID
-		else
-		echo "RecPro.sh no se estaba ejecutando."
-		echo "El comando RecPro.sh no se estaba ejecutando."
-		fi
-}
-stop
+functionToStop="$(ps -a | grep "$1" | awk '{print $1}')"
+
+if [ "$functionToStop" = '' ]
+then
+	echo "No se hay encontrado el proceso $1 corriendo"
+else
+	echo "Terminando proceso $1 con PID: $functionToStop"
+	kill $(ps -a | grep "$1" | awk '{print $1}')
+fi
